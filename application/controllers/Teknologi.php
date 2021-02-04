bcf5eada0a2ac976c90c42794b833fae672884e8
ijonk7 <jookeo.rizal@gmail.com>
2021-02-05 00:27:53 +0700

    Update url login social media

--- application/controllers/Teknologi.php
+++ application/controllers/Teknologi.php
@@ -24,7 +24,7 @@ class Teknologi extends CI_Controller {
 			$helper = $fb->getRedirectLoginHelper();
 				
 			$permissions = ['email']; // Optional permissions
-			$login_url_facebook = $helper->getLoginUrl('https://e-news.muhammad-rizal.com/login_fb', $permissions);
+			$login_url_facebook = $helper->getLoginUrl('https://e-news.muhammad-rizal.dev/login_fb', $permissions);
 		}
 		// End Code: Login With Facebook
 
@@ -35,7 +35,7 @@ class Teknologi extends CI_Controller {
         	// init configuration
         	$clientID = '291510143353-g6pgkgn2irhggg0gqchch7go9v2rovtu.apps.googleusercontent.com';
         	$clientSecret = 'wmKyldKQVc8Gc6lpv8qJzVAK';
-        	$redirectUri = 'https://e-news.muhammad-rizal.com/login_google';
+        	$redirectUri = 'https://e-news.muhammad-rizal.dev/login_google';
 
         	// create Client Request to access Google API
         	$client = new Google_Client();
@@ -77,7 +77,7 @@ class Teknologi extends CI_Controller {
 			$helper = $fb->getRedirectLoginHelper();
 				
 			$permissions = ['email']; // Optional permissions
-			$login_url_facebook = $helper->getLoginUrl('https://e-news.muhammad-rizal.com/login_fb', $permissions);
+			$login_url_facebook = $helper->getLoginUrl('https://e-news.muhammad-rizal.dev/login_fb', $permissions);
 		}
 		// End Code: Login With Facebook
 
@@ -88,7 +88,7 @@ class Teknologi extends CI_Controller {
         	// init configuration
         	$clientID = '291510143353-g6pgkgn2irhggg0gqchch7go9v2rovtu.apps.googleusercontent.com';
         	$clientSecret = 'wmKyldKQVc8Gc6lpv8qJzVAK';
-        	$redirectUri = 'https://e-news.muhammad-rizal.com/login_google';
+        	$redirectUri = 'https://e-news.muhammad-rizal.dev/login_google';
 
         	// create Client Request to access Google API
         	$client = new Google_Client();
